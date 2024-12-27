ccd84d0d90990c8ca646e799b89d77fba525ca9a
Hasan <hasan.raza@accionlabs.com>
2024-12-27 18:03:38 +0530

    shape get API

--- src/controllers/shape.controller.ts
+++ src/controllers/shape.controller.ts
@@ -3,6 +3,8 @@ import { ShapeService } from '../services/shape.service';
 import { NextFunction, Request, Response } from 'express';
 import { Controller, Get, Post } from '../core'
 import { ValidShape } from '../validations/shape.validation';
+import { CategoryService } from '../services/categories.service';
+import { ObjectId } from 'mongodb';
 
 @Service() // Marks this class as injectable
 @Controller('/shape')
@@ -10,6 +12,9 @@ export default class ShapeController {
 
   @Inject(() => ShapeService)
   private readonly shapeService: ShapeService
+
+  @Inject(() => CategoryService)
+  private readonly categoryService: CategoryService
 
   @Get('/', {
     isAuthenticated: true,
@@ -33,6 +38,23 @@ export default class ShapeController {
         const newShape = await this.shapeService.create(req.body);
         res.status(201).json(newShape);
     } catch(e) {
+        next(e)
+    }
+  }
+
+  @Get('/category/:categoryId', {
+    isAuthenticated: true,
+    authorizedRole: 'all'
+  })
+  async getShapesByCategory(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const { categoryId } = req.params;
+        const childCategories = await this.categoryService.getChildrenCategories(categoryId)
+        const categoriesTobeSearched: ObjectId[] = [new ObjectId(categoryId)]
+        childCategories.forEach(chCategory => categoriesTobeSearched.push(chCategory._id))
+        const shapes = await this.shapeService.findAll();
+        res.json(shapes);
+    } catch (e) {
         next(e)
     }
   }
