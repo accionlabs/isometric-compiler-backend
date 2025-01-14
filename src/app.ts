b2bcdff82011234a96e95bd880d2fe8ab9a6a2de
Hasan <hasan.raza@accionlabs.com>
2025-01-14 17:16:11 +0530

    extended json limit

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,8 @@ const app = express()
 
 app.use(morganLogger('dev'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 
 
 
