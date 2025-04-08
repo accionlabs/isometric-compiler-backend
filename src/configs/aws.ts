77aa9b99b380aed04a5b9ce6572849697168d5f5
Sayali Borade <sayali.borade@accionlabs.com>
2025-04-08 17:49:12 +0530

    us-west-2 region

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    AWS_REGION: process.env.AWS_REGION || "",
+    AWS_REGION: process.env.AWS_REGION || "us-west-2",
     AWS_S3_BUCKET: process.env.AWS_S3_BUCKET || "",
     AWS_ACCESS_KEYID: process.env.AWS_ACCESS_KEYID || "",
     AWS_SECRET_KEY: process.env.AWS_SECRET_KEY || "",
