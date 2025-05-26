6d7bda508e0afc397b52fe797e0c9499d7f9c645
Sayali Borade <sayali.borade@accionlabs.com>
2025-05-26 12:39:41 +0530

    qum spec selected in query

--- src/controllers/sematicModelHistory.controller.ts
+++ src/controllers/sematicModelHistory.controller.ts
@@ -5,6 +5,7 @@ import ApiError from "../utils/apiError";
 import { SemanticModelHistory } from "../entities/semantic_model_history.entity";
 import { SemanticModelHistoryService } from "../services/semanticModelHistory.service";
 import { FilterUtils } from "../utils/filterUtils";
+import { FindOptionsSelect } from "typeorm";
 
 @Service()
 @Controller('/semantic-model-history')
@@ -28,7 +29,7 @@ export default class SematicModelHistoryController {
             }
             const allowedFields: (keyof SemanticModelHistory)[] = ['createdAt', 'userId', "updatedAt", "status", "uuid"];
 
-            const selectedFields = { _id: true, uuid: true, createdAt: true, updatedAt: true, status: true, agent: true }
+            const selectedFields = { _id: true, uuid: true, createdAt: true, updatedAt: true, status: true, agent: true, qum_specs: true } as unknown as FindOptionsSelect<SemanticModelHistory>;
             const filters = FilterUtils.buildPostgresFilters<SemanticModelHistory>(restQuery, allowedFields);
             const sort: Record<string, 1 | -1> = { [sortName as string]: sortOrder === 'asc' ? 1 : -1 };
 
